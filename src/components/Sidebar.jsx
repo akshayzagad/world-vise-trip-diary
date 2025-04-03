56eb2d496d5fd12d41352b08c374adeb2d738a0b
akshayzagad <akshayzagade62@gmail.com>
2025-04-03 18:15:18 +0530

    add Sidebar component with logo, navigation, and footer

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import Logo from './Logo'
+import styles from './Sidebar.module.css'
+import AppNav from './AppNav.jsx'
+
+function Sidebar() {
+    return (
+        <div className={styles.sidebar}>
+            <Logo/>
+            <AppNav/>
+            <p>List of cities</p>
+            <footer className={styles.footer}>
+              <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear()} by WorldTour.inc.</p>  
+            </footer>
+        </div>
+    )
+}
+
+export default Sidebar
