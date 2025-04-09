83a94158edace88e760f952ce72248fd582739ed
akshayzagad <akshayzagade62@gmail.com>
2025-04-09 19:22:48 +0530

    Refactor Map component; update map position handling, enhance zoom and scroll settings, and integrate ChangeCenter for dynamic view adjustments

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import styles from "./Map.module.css";
 import { useState, useEffect } from "react";
 import { useCities } from "../../Context/CitiesContext";
@@ -7,39 +7,51 @@ import { useCities } from "../../Context/CitiesContext";
 function Map() {
   const navigate = useNavigate();
   const [searchParam] = useSearchParams();
-  const {cities} = useCities();
-  const [mapPosition, setMapPosition] = useState([51.505, -0.09]); // Default position
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]); // Default position
 
-  const lat = searchParam.get("lat");
-  const lng = searchParam.get("lng");
+  const mapLat = searchParam.get("lat");
+  const mapLng = searchParam.get("lng");
 
   useEffect(() => {
-    if (lat && lng) {
-      setMapPosition([parseFloat(lat), parseFloat(lng)]); // Update position if lat/lng are available
+    if (mapLat && mapLng) {
+      setMapPosition([mapLat, mapLng]); // Update position if lat/lng are available
     }
-  }, [lat, lng]);
+  }, [mapLat, mapLng]);
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition} // Ensure center is always valid
-        zoom={13}
-        scrollWheelZoom={false}
+        zoom={6}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
 
-        {cities.map((city)=><Marker position={[city.position.lat,city.position.lng]} key={city.id}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>)}
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
