b5b2d4399df3af06454719f931cc0a88de53f237
akshayzagad <akshayzagade62@gmail.com.com>
2025-04-09 14:15:44 +0530

    Remove unused city entries from cities.json; update Map component to utilize CitiesContext for city markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,10 +2,12 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
 import { useState, useEffect } from "react";
+import { useCities } from "../../Context/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
   const [searchParam] = useSearchParams();
+  const {cities} = useCities();
   const [mapPosition, setMapPosition] = useState([51.505, -0.09]); // Default position
 
   const lat = searchParam.get("lat");
@@ -29,11 +31,12 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
+
+        {cities.map((city)=><Marker position={[city.position.lat,city.position.lng]} key={city.id}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker>)}
       </MapContainer>
     </div>
   );
